8d56d15bb0bdbe995461b17ae1fefb263289719b
joao0otavio3@gmail.com <joao0otavio3@gmail.com>
2022-11-18 22:49:25 -0300

    extrai header para componente

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import './App.css';
-import SearchIcon from '@mui/icons-material/Search';
-import AccessibilityNewRoundedIcon from '@mui/icons-material/AccessibilityNewRounded';
-import LocalGroceryStoreOutlinedIcon from '@mui/icons-material/LocalGroceryStoreOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Header from './components/Header';
+
 import Evento from './Evento'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       
-      <header>
-        
-        <div id='logo'>USParty</div>
-
-        <div id="searchBar">
-          <input placeholder="Proucure um evento"/>
-          <SearchIcon className='searchIcon'/>
-        </div>
-
-        <div id='actions'>
-          <AccessibilityNewRoundedIcon className='accessibilityIcon' style={{ fontSize: 40 }}  />
-          <LocalGroceryStoreOutlinedIcon className='carrinho' fontSize='medium' style={{ fontSize: 60 }} />
-          <AccountCircleOutlinedIcon className='login' fontSize='large' style={{ fontSize: 60 }} />
-        </div>
-
-      </header>
 
       <section id='maisVendidos'>
         <h1>Mais Vendidos</h1>
---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import AccessibilityNewRoundedIcon from '@mui/icons-material/AccessibilityNewRounded';
+import LocalGroceryStoreOutlinedIcon from '@mui/icons-material/LocalGroceryStoreOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
+const Header = () => {
+  return (
+    <header>
+        <div id='logo'>USParty</div>
+        
+        <div id="searchBar">
+          <input placeholder="Proucure um evento"/>
+          <SearchIcon className='searchIcon'/>
+        </div>
+
+        <div id='actions'>
+          <AccessibilityNewRoundedIcon className='accessibilityIcon' style={{ fontSize: 40 }}  />
+          <LocalGroceryStoreOutlinedIcon className='carrinho' fontSize='medium' style={{ fontSize: 60 }} />
+          <AccountCircleOutlinedIcon className='login' fontSize='large' style={{ fontSize: 60 }} />
+        </div>
+  </header>
+  );
+}
+
+export default Header;
