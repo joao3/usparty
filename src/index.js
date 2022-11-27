67eed83e4260766737053dc134a81da343c7535d
LucasBetoni <lucasbetoni@gmail.com>
2022-11-27 18:53:38 -0300

    Fix dependency

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import MinhaConta from './pages/MinhaConta/MinhaConta';
+import DetalheEvento from './pages/DetalheEvento/DetalheEvento';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/*<DetalheEvento />*/}
+    {/*<MinhaConta />*/}
     <App />
   </React.StrictMode>
 );
