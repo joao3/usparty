99f800bc7843e4d5cf73f8f7b094cc87a7649167
LucasBetoni <lucasbetoni@gmail.com>
2022-12-01 15:53:08 -0300

    added router
    paginas acessiveis pela barra de endereço
    Ex
    '/' -> home
    '/minhaconta'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
-import MinhaConta from './pages/MinhaConta/index';
+import MinhaConta from './pages/MinhaConta';
 import DetalheEvento from './pages/DetalheEvento';
+import Pagamento from './pages/Pagamento';
+import Login from './pages/Login';
+import Carrinho from './pages/Carrinho';
+import Cadastrar from './pages/Cadastrar';
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },{
+    path: "/minhaconta",
+    element: <MinhaConta />,
+  },{
+    path: "/pagamento",
+    element: <Pagamento />,
+  },{
+    path: "/login",
+    element: <Login />,
+  },{
+    path: "/detalheevento",
+    element: <DetalheEvento />,
+  },{
+    path: "/carrinho",
+    element: <Carrinho />,
+  },{
+    path: "/cadastrar",
+    element: <Cadastrar />,
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<DetalheEvento />*/}
-    {/*<MinhaConta />*/}
-    <Home />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
