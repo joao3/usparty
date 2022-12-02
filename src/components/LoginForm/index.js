0f8569e8f09e9e94d2a19c45dd8607e70e0eaa47
LucasBetoni <lucasbetoni@gmail.com>
2022-12-02 20:39:20 -0300

    adiciona pagina cadastro

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -3,6 +3,7 @@ import Input from '../Input';
 import { useAuth } from '../../context/Auth';
 import { redirect } from 'react-router-dom';
 
+
 const Container = styled.form`
     max-width: 600px;
     margin: 0 auto;
@@ -19,6 +20,17 @@ const Container = styled.form`
         font-size: 32px;
         cursor: pointer;
     }
+`;
+const termos = styled.form`
+align-self: flex-end;
+margin-left: 40px;
+font-size: 60px;
+}
+`;
+const termos2 = styled.form`
+align-self: flex-end;
+font-size: 60px;
+}
 `;
 
 const lock = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
@@ -41,6 +53,8 @@ const LoginForm = (props) => {
             <Input icon={email} placeholder="exemplo@email.com" type="text" fieldId="login-name" label="Email" />
             <Input icon={lock} placeholder="•••••" type="password" fieldId="login-password" label="Senha"/>
             <Input type="submit" value="Enviar"/>
+            <span className="termos">
+            Não tem conta? </span><a className="termos2" href='/cadastrar'>Cadastre-se.</a>
         </Container>
     );
 }
