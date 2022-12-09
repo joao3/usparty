8e413e14fe0cb609a2a2a5ee16f2785d916c391a
Eduardo Maciel <54045014+edumaciel10@users.noreply.github.com>
2022-12-09 17:43:34 -0300

    feat(breadcrumb): breadcrumb (#20)

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -3,15 +3,54 @@ import Header from "../Header";
 import React from "react";
 import { AuthProvider } from "./../../context/Auth";
 import ScrollToTop from "../ScrollToTop";
+import { Link, Route, Router, useLocation } from "react-router-dom";
+import { Box, Breadcrumbs, Typography } from "@mui/material";
+import { Container } from "../Container";
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
+function SimpleBreadcrumbs() {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+  console.log({ location, pathnames });
+  return (
+    <Container sx={{ml: "10px"}}>
+      <Box sx={{ml: "20px", mt: "20px"}}>
+        <Breadcrumbs aria-label="Breadcrumb"
+        separator={<NavigateNextIcon fontSize="small" />}>
+          <Link color="inherit" to="/">
+            Home
+          </Link>
+          {pathnames.map((value, index) => {
+            const last = index === pathnames.length - 1;
+            const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+            const formatedValue = value.toLocaleLowerCase()[0].toUpperCase() + value.slice(1)
+
+            return last ? (
+              <Typography color="textPrimary" key={to}>
+                {formatedValue}
+              </Typography>
+            ) : (
+              <Link color="inherit" to={to} key={to}>
+                {formatedValue}
+              </Link>
+            );
+          })}
+        </Breadcrumbs>
+      </Box>
+    </Container>
+  );
+}
 export function Wrapper(children) {
   return (
-    <AuthProvider>
-      <ScrollToTop>
-        <Header />
-        {children}
-        <Footer />
-      </ScrollToTop>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <ScrollToTop>
+          <Header />
+          <SimpleBreadcrumbs />
+          {children}
+          <Footer />
+        </ScrollToTop>
+      </AuthProvider>
+    </>
   );
 }
